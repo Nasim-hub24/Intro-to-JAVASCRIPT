b7c6443a38753b1493b34ded19596b34b0b5d4ba
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-03-22 20:25:00 +0600

    string and boolean type

--- Introduction_to_javascript/first.js
+++ Introduction_to_javascript/first.js
@@ -1,5 +1,5 @@
 
-// Introduction to Javascript 
+// Variable declaration 
 var price = 100;
 var age = 20;
 console.log(price);
@@ -9,3 +9,15 @@ var weight = 200;
 //FOR UPDATING THE VALUE OF WEIGHT
 weight = 500;
 console.log(weight);
+
+// string
+var name = "no name";
+console.log(name);
+var adress = "dubai";
+console.log(adress);
+
+// boolean 
+var bool = true;
+console.log(bool);
+var fake = false;
+console.log(typeof fake);
