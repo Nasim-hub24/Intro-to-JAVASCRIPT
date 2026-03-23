448851a7ac06116becbcc37101995667279600ba
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-03-24 01:01:25 +0600

    use and or inside the condition

--- Introduction_to_javascript/conditionals.js
+++ Introduction_to_javascript/conditionals.js
@@ -5,11 +5,30 @@ const salary = 2000;
 const age = 26;
 const height = 56;
 
-if(salary > 30000 && age >=26)
+ if(salary > 30000 && age >=26)
+     {
+     console.log('su----patro');
+    }
+
+ else
+    {
+        console.log('not perfect');
+    }
+
+ if(salary > 1000 || height >=56)
+    {
+        console.log('happy');
+    }
+ else{
+     console.log('tani tuni sola lagbo');
+    }
+
+// ------- COMPLEX CONDITIONALS ------
+
+if((salary>=20000 && height>=56) && (salary> 30000 || age>=26))
 {
-    console.log('su----patro');
+    console.log('happy');
 }
-else
-{
-    console.log('not perfect');
+else{
+    console.log('not happy');
 }
