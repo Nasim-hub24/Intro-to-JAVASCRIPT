8e2e62bb67609f77b3eb582c0f261c7c45d77039
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-03-24 01:48:56 +0600

    use  ternary operator to semplyfy the code

---
+++ Introduction_to_javascript/ternarry_operator.js
@@ -0,0 +1,21 @@
+
+/**
+ * ---- ternary means three part ------
+ * 
+ * condition ? do something when true : do something when false;
+ */
+
+const age = 17;
+
+// if(age >= 18)
+// {
+//     console.log('you can vote');
+// }
+// else
+// {
+//     console.log('you cannot vote');
+// }
+
+// USING TERNARY OPERATOR
+
+age >= 18 ? console.log('you can vote') : console.log('you cannot vote');
