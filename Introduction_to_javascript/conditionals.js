3032490aeef52d6228929794ffc065d7bb190849
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-03-24 00:52:40 +0600

    simple condition

---
+++ Introduction_to_javascript/conditionals.js
@@ -0,0 +1,15 @@
+
+// simple condition 
+// const means fixed value 
+const salary = 2000;
+const age = 26;
+const height = 56;
+
+if(salary > 30000 && age >=26)
+{
+    console.log('su----patro');
+}
+else
+{
+    console.log('not perfect');
+}
