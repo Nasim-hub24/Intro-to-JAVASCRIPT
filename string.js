8ff7e1b8596c753faf358e88e7334592583b6043
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-04 02:01:13 +0600

    lower case and uper case

--- string.js
+++ string.js
@@ -11,3 +11,13 @@ const numbers = [10, 15, 29, 40];
 console.log( numbers.length);
 console.log(numbers[2]);
 console.log(numbers);
+
+// UPPER CASE AND LOWER CASE
+
+const school = 'Sjiscs';
+const college = 'SchOLarshome';
+
+console.log(school);
+console.log(school.toLocaleLowerCase());
+console.log(college.toUpperCase());
+console.log(college);
