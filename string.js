214206634f4920fa26adfaada933e1ce1e4f2138
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-04 00:44:20 +0600

    basic string concept

---
+++ string.js
@@ -0,0 +1,13 @@
+// STRING IN JAVASCRIPT
+
+const country = 'bangladesh';
+const name = "sylhet";
+const dapot = "amra hokkol sylothi";
+
+console.log(name);
+console.log(typeof dapot);
+
+const numbers = [10, 15, 29, 40];
+console.log( numbers.length);
+console.log(numbers[2]);
+console.log(numbers);
