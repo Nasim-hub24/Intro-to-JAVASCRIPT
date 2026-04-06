100d7985c71e43516ba3b847c3037fd1187ec8ba
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-07 00:58:42 +0600

    basic stracture of a function

--- Introduction_to_javascript/functions.js
+++ Introduction_to_javascript/functions.js
@@ -5,3 +5,14 @@
  *     a task when it is called.
  * --< maintains relationship with input and output.
  */
+
+// HOW TO DECLEAR A FUNCTION
+
+const value = 50;
+function FanOff(){
+    console.log('bosa takay utan');
+    console.log('towards the swich');
+    console.log('off the swich');
+    console.log('kal katham');
+}
+FanOff(); // for print the function 
