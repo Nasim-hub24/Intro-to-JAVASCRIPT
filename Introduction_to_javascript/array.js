67d75bca8067c320af09c63b54ab6939b2999eec
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-06 23:10:17 +0600

    we use for of to loop an array

---
+++ Introduction_to_javascript/array.js
@@ -0,0 +1,16 @@
+
+/**
+ * loop tenchnique 
+ * 1. for 
+ * 2. while
+ * 3. for in --> object loop korar jono
+ * 4. for of --> array loop korar jono
+ */
+
+// for of 
+const friends = ['kashem sulaimane', 'ali khamenie', 'abbas agrachi'];
+
+for (const friend of friends)
+{
+    console.log(friend);
+}
