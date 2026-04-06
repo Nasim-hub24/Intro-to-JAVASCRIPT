f099222d30ed46ac149519518e7657b1acfb762f
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-07 00:24:23 +0600

    simple reverse printing

--- Introduction_to_javascript/array.js
+++ Introduction_to_javascript/array.js
@@ -8,9 +8,31 @@
  */
 
 // for of 
+/**
+ 
 const friends = ['kashem sulaimane', 'ali khamenie', 'abbas agrachi'];
+
+console.log(friends.length);
 
 for (const friend of friends)
 {
     console.log(friend);
 }
+
+for(let i=0; i<friends.length;i++){
+    console.log (friends[i]);
+    console.log(i);
+}
+ */     
+
+// Reverse an array
+
+const numbers = [ 10 , 20 ,30 ,40];
+console.log(numbers);
+
+const rev_num = [];
+
+for(const number of numbers){
+    rev_num.unshift(number);
+}
+console.log(rev_num);
