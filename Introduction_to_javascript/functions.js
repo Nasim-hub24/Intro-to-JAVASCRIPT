00120647c7a80d6dba47af3ae281e9bc11301e21
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-07 00:35:12 +0600

    theory of functuon

---
+++ Introduction_to_javascript/functions.js
@@ -0,0 +1,7 @@
+
+/** 
+ * -------- WHAT IS FUNCTION IN JS --------
+ * --< function is a block of code or a set of statements that performs 
+ *     a task when it is called.
+ * --< maintains relationship with input and output.
+ */
