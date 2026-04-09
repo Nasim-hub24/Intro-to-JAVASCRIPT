f9e75a07e608f8e7bb60cea20280945ce76ffab7
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-09 21:01:13 +0600

    return in function

---
+++ Introduction_to_javascript/return.js
@@ -0,0 +1,13 @@
+
+/**
+ * return → sends value back
+ * stops function execution
+ */
+
+function tentimes(number){
+    const multiply = number*10;
+    return multiply; // kala ono ow kotom nisa ar joto code lako na kana it doesn't exicute
+}
+
+const output = tentimes(10);
+console.log('output is:', output);
