739544b16193c0dc38745303ab3181507859c1df
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-10 23:24:15 +0600

    find odd and even usin return in function

--- Introduction_to_javascript/types_of_parameters.js
+++ Introduction_to_javascript/types_of_parameters.js
@@ -17,3 +17,43 @@ function evenSize(str){
 }
 evenSize('dhaka');
 evenSize('faka');
+
+// -----------<Sum of an array using function>--------------
+
+function sumNumber(numbers){
+    let sum = 0;
+    for(const number of numbers){
+        console.log(number);
+        sum = sum+number;
+    }
+    return sum;
+}
+
+const numbs = [20, 30, 40, 50];
+const sum = sumNumber(numbs);
+console.log('total sum is: ', sum);
+
+// ---------------<find the even and odd numbers>------------------
+
+function evenOnly(numbers){
+
+    const evens = [];
+    const odds = [];
+    for(const number of numbers){
+        if(number%2 ===0){
+            console.log(number);
+            evens.push(number);
+        }
+        else{
+            odds.push(number);
+        }
+    }
+    return {evens, odds};
+}
+
+const numbers = [8, 9, 6, 40 ,3];
+const evens = evenOnly(numbers);
+// const odds = evenOnly(numbers);
+console.log('even numbers are: ', evens);
+// console.log('odd number are: ', odds);
+
