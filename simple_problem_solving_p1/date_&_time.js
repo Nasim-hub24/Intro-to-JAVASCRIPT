4c9d94ff6c991d12abb164d5361151a39eb8f6dc
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-13 12:51:47 +0600

    swaping numbers

---
+++ simple_problem_solving_p1/date_&_time.js
@@ -0,0 +1,17 @@
+
+/**
+ * const today = new Date();
+ */
+
+let a = 5;
+let b = 10;
+console.log(a,b);
+
+//----------------------//
+
+// SWAP THE NUMBER
+const temp = a;
+a = b;
+b = temp;
+console.log(a,b);
+
