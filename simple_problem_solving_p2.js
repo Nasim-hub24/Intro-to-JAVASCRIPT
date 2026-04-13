1c8647f1d01233c6939eff393186baf1e2cda8d0
Nasim-heb24 <ahmednasim2487@gmail.com>
2026-04-13 13:26:37 +0600

    simple code for findig maximum number

---
+++ simple_problem_solving_p2.js
@@ -0,0 +1,40 @@
+/**
+ *  find the maximum between two number
+ *  using function
+ */
+
+function getMex(num1 , num2)
+{
+    if(num1 > num2)
+    {
+        return num1;
+    }
+    else
+    {
+        return num2;
+    }
+}
+const max = getMex(70, 60);
+console.log('max of the two number: ', max);
+
+// find the maximum between three number
+// using function
+
+function getMaximum(num1, num2, num3)
+{
+    if(num1>num2 && num1>num3)
+    {
+        return num1;
+    }
+    else if(num2> num1 && num2>num3)
+    {
+        return num2;
+    }
+    else
+    {
+        return num3;
+    }
+}
+
+const maximum = getMaximum(60, 70,80)
+console.log(maximum);
